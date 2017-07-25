7e5de2c60f8bfc95dda043b7441c31b8ab3a823f
Truman Purnell <truman.purnell@gmail.com>
2017-07-25 16:06:49 -0400

    simplifies boilerplate

---
+++ app/components/Routes.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+class Routes extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+
+                </div>
+            </Router>
+        );
+    }
+}
+
