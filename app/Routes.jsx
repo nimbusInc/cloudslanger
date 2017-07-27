a49f33edb653a840d624004731a17b62345a4bf6
andrew-atkinson <blind.sam@gmail.com>
2017-07-27 11:12:35 -0400

    chore: cleaning up routes.jsx

--- app/Routes.jsx
+++ app/Routes.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
-console.log('foo');
-import AllProducts from "./components/AllProducts";
-import { retrieveProducts } from "./reducers/products";
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+console.log('foo')
+import AllProducts from './components/AllProducts'
+import {retrieveProducts} from './reducers/products'
 import Jumbotron from './components/Jumbotron'
 
 class Routes extends Component {
@@ -16,22 +16,22 @@ class Routes extends Component {
     render() {
         return (
             <Router>
-                <div className="culmn">
-                    {" "}{/*this is the way god wants it...*/}
-                    <Navbar />
-                    <Route exact path="/" component={Home} />
-                    <Route path="/jumbotron" component={Jumbotron} />
-                    <Route path="/products" component={AllProducts} />
+                <div className='culmn'>
+                    {''}{/* this is the way god wants it... */}
+                    <Navbar/>
+                    <Route exact path='/' component={Home}/>
+                    <Route path='/jumbotron' component={Jumbotron}/>
+                    <Route path='/products' component={AllProducts}/>
                 </div>
             </Router>
-        );
+        )
     }
 }
 
 const mapDispatch = dispatch => ({
     fetchInitialData: () => {
-        dispatch(retrieveProducts());
+        dispatch(retrieveProducts())
     }
-});
+})
 
-export default connect(null, mapDispatch)(Routes);
+export default connect(null, mapDispatch)(Routes)
