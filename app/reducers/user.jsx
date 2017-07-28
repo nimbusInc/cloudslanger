0cbc151b62ae7faedee1122fabc65d31cd12407e
Truman Purnell <truman.purnell@gmail.com>
2017-07-28 16:39:05 -0400

    feature: adds redux functionality for cart

---
+++ app/reducers/user.jsx
@@ -0,0 +1,83 @@
+import axios from 'axios'
+
+const reducer = (state={user: {}, cart: {}}, action) => {
+    const newState = Object.assign({}, state)
+    
+    switch (action.type) {
+      
+      case AUTHENTICATED:
+          newState.user = action.user
+          return newState
+
+      case ADD_TO_CART:
+          const cart = newState.cart
+          const item = action.item
+          cart[item.id] 
+          ? cart[item.id] += item.quantity
+          : cart[item.id] = item.quantity
+          return newState
+
+      case REMOVE_FROM_CART:
+          const cart = newState.cart
+          cart[action.item.id] = null
+          return newState
+
+      case UPDATE_ITEM:
+          const cart = newState.cart
+          cart[action.item.id] = action.item.newQuantity
+          return newState
+      
+    }
+
+    return newState
+}
+
+
+const ADD_TO_CART = 'atc'
+export const addToCart = item => ({
+  type: ADD_TO_CART, action: item 
+})
+
+
+const REMOVE_FROM_CART = 'rfc'
+export const removeFromCart = item => ({
+  type: REMOVE_FROM_CART, action: item 
+})
+
+const UPDATE_ITEM = 'ui'
+export const updateItem = item => ({
+  type: UPDATE_ITEM, action: item 
+})
+
+const CHECKOUT = 'co'
+
+
+
+const AUTHENTICATED = 'AUTHENTICATED'
+export const authenticated = user => ({
+    type: AUTHENTICATED, user
+})
+
+export const login = (username, password) =>
+  dispatch =>
+    axios.post('/api/auth/login/local',
+      {username, password})
+      .then(() => dispatch(whoami()))
+      .catch(() => dispatch(whoami()))
+
+export const logout = () =>
+  dispatch =>
+    axios.post('/api/auth/logout')
+      .then(() => dispatch(whoami()))
+      .catch(() => dispatch(whoami()))
+
+export const whoami = () =>
+  dispatch =>
+    axios.get('/api/auth/whoami')
+      .then(response => {
+          const user = response.data
+          dispatch(authenticated(user))
+      })
+      .catch(failed => dispatch(authenticated(null)))
+
+export default reducer
