928a3cda83a3d12381f7081bf52ec784b3f8ed22
Justin Pinero <justinpinero@gmail.com>
2017-07-28 15:26:47 -0400

    fix all products filter

--- app/components/AllProducts.jsx
+++ app/components/AllProducts.jsx
@@ -34,10 +34,9 @@ function AllProducts(props) {
                                 <div className="col-md-12 m-bottom-60">
                                     <div className="filters-button-group text-right sm-text-center">
                                         <button className="button is-checked" onClick={() => category='all'}>View all</button>
-                                        <button className="button" onClick={(evt) => {
-                                            console.log('this is working', category)
-                                            return category='clouds'
-                                        }}>Clouds</button>
+                                        <button className="button" onClick={() => {
+                                          console.log("this is working");
+                                          return category}='clouds'}>Clouds</button>
                                         <button className="button" onClick={() => category='fog'}>Fog</button>
                                         <button className="button" onClick={() => category='storms'}>Storms</button>
                                         <button className="button" onClick={() => category='rain'}>Rain</button>
