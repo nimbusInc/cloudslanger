02215a07fb9e30f226079c9008bc9edc06a5be77
Eli Mauskopf <elimauskopf7@gmail.com>
2017-07-28 10:58:16 -0400

    added backend test

--- server/backend.test.js
+++ server/backend.test.js
@@ -2,58 +2,54 @@
 
 const db = require('./app/db')
 , { User, Product, Review, Order } = db
-, {expect} = require('chai')
+, {expect, describe, before, beforeEach, afterEach, it} = require('chai')
 
-describe('The Article Model', () => {
-    
+describe('The User Model', () => {
     before(function() {
         return db.sync({force: true})
     })
 
-  var user;
-  beforeEach(function(){
-    user = User.build({
-      name: 'Barack Obama',
-      email: 'some@example.com'
-    });
+    var user
+    beforeEach(function() {
+        user = User.build({
+            name: 'Barack Obama',
+            email: 'some@example.com'
+        })
 
-    afterEach(function () {
-    return Promise.all([
-      User.truncate({ cascade: true }) 
-    ])
-  })
-  })
+        afterEach(function() {
+            return Promise.all([
+                User.truncate({ cascade: true })
+            ])
+        })
+    })
 
     describe('attributes definition', function() {
-        
-        it('includes name and email fields', function(){
+        it('includes name and email fields', function() {
             return user.save()
             .then(function(savedUser) {
                 expect(savedUser.name).to.equal('Barack Obama')
-                excpet(savedUser.email).to.equal('some@example.com')
-            } )
+                expect(savedUser.email).to.equal('some@example.com')
+            })
         })
 
-        it('requires name', function(){
+        it('requires name', function() {
             user.name = null
-            
 
             return user.validate()
-            .then(function(result){
+            .then(function(result) {
                 expect(result).to.be.an.instanceOf(Error)
                 expect(result.message).to.contain('Validation error')
             })
         })
 
-        it('requires email', function(){
+        it('requires email', function() {
             user.email = null
-            
 
             return user.validate()
-            .then(function(result){
+            .then(function(result) {
                 expect(result).to.be.an.instanceOf(Error)
                 expect(result.message).to.contain('Validation error')
             })
         })
     })
-  })
+})
