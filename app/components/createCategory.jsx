0b11f95770213846934a9ec3a077842fb4f3d88d
Justin Pinero <justinpinero@gmail.com>
2017-07-31 18:37:14 -0400

    creates addCategory component

---
+++ app/components/createCategory.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {NavLink, withRouter} from 'react-router-dom'
+import {addCategory} from './reducers/categories'
+
+class adminAddCategory extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state= {
+            name: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event) {
+        if (event.target.name === 'name') {
+            this.setState({name: event.target.value})
+        }
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        const newCategory = {
+            name: this.state.name
+        }
+        this.props.addCategory(newCategory)
+        this.setState({
+            name: ''
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.campusName}</h1>
+                <form onSubmit= {this.handleSubmit}>
+                    <div className="form-group">
+                        <label>NAME</label>
+                        <input type="text" value={this.state.name} onChange={this.handleChange} id="name-field" name="name" />
+                    </div>
+                    <button type="submit">Add Category</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+const mapProps= (state) => ({
+    categories: state.categories
+})
+
+const mapDispatch = {addCategory}
+
+export default connect(mapProps, mapDispatch)(adminAddCategory)
