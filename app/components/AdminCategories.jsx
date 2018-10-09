1a8d96176cbefa7840b652e052e12f998fe66071
andrew-atkinson <blind.sam@gmail.com>
2018-10-09 19:13:51 -0400

    changes: tidied up component

--- app/components/AdminCategories.jsx
+++ app/components/AdminCategories.jsx
@@ -5,26 +5,30 @@ import {connect} from 'react-redux'
 class AdminCategories extends React.Component {
     render() {
         const categoryList = this.props.categories
-        return (< div className = "row" > {
-            categoryList && categoryList.map((category) => (
-                <div className="text-center" key={category.id}>
-                    <NavLink to={`admin/categories/${category.id}`}>
-                        <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12 ">
-                            <img alt="" src={category.img}></img>
-                            <div className=" text-center">
-                                <h4 >{category.name}</h4>
-                            </div>
+        return (
+            <div className="row"> 
+                {
+                    categoryList && categoryList.map(category => (
+                        <div className="text-center" key={category.id}>
+                            <NavLink to={`admin/categories/${category.id}`}>
+                                <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12 ">
+                                    <img alt="" src={category.img}></img>
+                                    <div className=" text-center">
+                                        <h4 >{category.name}</h4>
+                                    </div>
+                                </div>
+                            </NavLink>
                         </div>
-                    </NavLink>
-                </div>
-            ))
-        } < NavLink to = {
-            `admin/addCategory`
-        } > ADD CATEGORY < /NavLink>
-        </div >)
+                    ))
+                } 
+                <NavLink to={`admin/addCategory`}>
+                 ADD CATEGORY 
+                </NavLink>
+            </div >
+        )
     }
 }
 
-const mapProps = (state) => ({categories: state.categories})
+const mapProps = state => ({categories: state.categories})
 
 export default connect(mapProps)(AdminCategories)
